ee7e3eba5f58713a2475df10436b6f626fc00585
daisuke.nakamura <dskdsk773@gmail.com>
2021-05-28 08:59:18 +0000

    コミットテスト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
     <div class="center jumbotron bg-warning">
         <div class="text-center text-white">
-            <h1>YouTubeまとめ × SNS</h1>
+            <h1>YouTubeまとめ × SNS (コミットテスト)</h1>
         </div>
     </div>
     
